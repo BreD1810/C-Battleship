505dddeddc02c3009f65197e0ac8b4a40c48ae3f
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2018-06-12 14:26:33 +0100

    Created a game loop - the player can choose to play another game when the current one is completed.

--- BattleshipConsole/BattleshipConsole/Program.cs
+++ BattleshipConsole/BattleshipConsole/Program.cs
@@ -1,14 +1,45 @@
 ﻿using System;
+using System.Threading;
 
 namespace BattleshipConsole
 {
 
-    class Program
+    //Create a game instance. When it is completed, loop if required.
+    static class Program
     {
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool loop = true;
+            do
+            {
+                Console.Clear();
+                Game game = new Game();
+                Console.WriteLine("Do you wish to play another game?");
+                bool check = true;
+                //Exit if no, new game if yes, loop until a valid input.
+                do
+                {
+                    Console.Write("Y/N: ");
+                    string answer = Console.ReadLine();
+                    if (answer.ToLower() == "n")
+                    {
+                        Console.WriteLine("Press enter to close the window...");
+                        loop = false;
+                        check = false;
+                    }
+                    else if (answer.ToLower() == "y")
+                    {
+                        Console.WriteLine("Starting a new game...");
+                        Thread.Sleep(2000);
+                        check = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter Y or N.");
+                    }
+                } while (check);
+            } while (loop);
         }
 
     }
