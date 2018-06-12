80c226a83f6a03cf48d1d32e6b0988e7cec50eb4
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2018-06-12 14:52:48 +0100

    Player names are now determined

--- BattleshipConsole/BattleshipConsole/Game.cs
+++ BattleshipConsole/BattleshipConsole/Game.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace BattleshipConsole
 {
+    //Represents an instance of a Battleship game
     class Game
     {
+
+        private static Player[] _players;
+
+        public Game()
+        {
+            _players = DeterminePlayers();
+        }
+
+        //Get the player names and create the objects
+        private static Player[] DeterminePlayers()
+        {
+            Player[] players = new Player[2];
+            
+            Console.WriteLine("Enter the first player's name:");
+            Console.Write("Name: ");
+            string name1 = Console.ReadLine();
+            players[0] = new Player(name1);
+            Console.WriteLine("Enter the second player's name:");
+            Console.Write("Name: ");
+            string name2 = Console.ReadLine();
+            players[1] = new Player(name2);
+
+            return players;
+        }
+
     }
 }
--- BattleshipConsole/BattleshipConsole/Player.cs
+++ BattleshipConsole/BattleshipConsole/Player.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace BattleshipConsole
 {
+    //Represents a player in the game
     class Player
     {
+
+        public string Name { get; }
+        private Board myBoard = new Board();
+        private Board guessBoard = new Board();
+
+        public Player(string name){ Name = name; }
+
     }
 }
