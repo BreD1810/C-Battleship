6d80f822c0667c5d8be08d0e4c9c5597a2610868
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2018-06-12 15:25:47 +0100

    The game boards can now be initiated and drawn to the console.

--- BattleshipConsole/BattleshipConsole/Board.cs
+++ BattleshipConsole/BattleshipConsole/Board.cs
@@ -6,5 +6,65 @@ namespace BattleshipConsole
 {
     class Board
     {
+
+        private const int BoardSize = 10;
+        private char[,] _board = new char[BoardSize, BoardSize];
+
+        public Board()
+        {
+            FillBoard();
+        }
+
+        //Set the default value of the board
+        private void FillBoard()
+        {
+            for (int i = 0; i < BoardSize; i++)
+            {
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    _board[i, j] = '-';
+                }
+            }
+            
+        }
+
+        //Draw the board
+        public void Draw()
+        {
+            //Method to get the top row of the board
+            string TopRow()
+            {
+                var builder = new StringBuilder("  ");
+
+                for (int i = 0; i < BoardSize; i++)
+                {
+                    builder.Append($"{i + 1,2}");
+                }
+
+                return builder.ToString();
+            }
+
+            Console.WriteLine(TopRow());
+            for (int i = 0; i < BoardSize; i++)
+            {
+                Console.WriteLine($"{i+1, 2}");
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    Console.Write('|');
+                    Console.Write(_board[i, j]);
+                }
+                Console.Write('|');
+                Console.WriteLine();
+            }
+
+            //Print the bottom of the board.
+            Console.Write("  ");
+            for (int i = 0; i < BoardSize; i++)
+            {
+                Console.Write("\u0305\u0305");
+            }
+            Console.WriteLine("\u0305");
+        }
+
     }
 }
