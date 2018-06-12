1667c3575b8a4f9e099f7792d1e8c8785e2ec070
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2018-06-12 15:42:21 +0100

    Gave ships length, hit recording and destruction boolean

--- BattleshipConsole/BattleshipConsole/Ship.cs
+++ BattleshipConsole/BattleshipConsole/Ship.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace BattleshipConsole
 {
+    //Represents a ship
     class Ship
     {
+
+        public int Length { get; }
+        public bool[] Hits { get; }
+        public bool Destroyed { get; private set; }
+
+        public Ship(int length)
+        {
+            Length = length;
+            Hits = new bool[length];
+        }
+
+        private bool CheckDestroyed()
+        {
+            foreach (bool hit in Hits)
+            {
+                if (!hit)
+                    return false;
+            }
+
+            Destroyed = true;
+            return true;
+        }
+
     }
 }
