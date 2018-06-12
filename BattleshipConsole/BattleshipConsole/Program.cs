2820fbf8e6098d34c5cedc60f7a2435ed52740dd
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2018-06-12 14:18:11 +0100

    Created necessary classes

--- BattleshipConsole/BattleshipConsole/Program.cs
+++ BattleshipConsole/BattleshipConsole/Program.cs
@@ -2,11 +2,14 @@
 
 namespace BattleshipConsole
 {
+
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
     }
 }
